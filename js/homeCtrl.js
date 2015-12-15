0de9c30a9d5e9d0b57ee23545e0e811840311992
PaulBrimley <paul.k.brimley@gmail.com>
2015-12-15 14:36:37 -0700

    completed 3rd day but has errors on localhost 8081

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -1,9 +1,4 @@
-angular.module('devMtIn').controller('homeCtrl', function($scope) {
-
-	$scope.myProfile =  {
-
-		friends: [{name: 'Eric'}, {name: 'Phil'}, {name: 'Landon'}, {name: 'Joe'}, {name: 'Jess'}]
-	};
+angular.module('devMtIn').controller('homeCtrl', function($scope, profileService) {
 
 	$scope.sortOptions = [{
 		display: 'Ascending',
@@ -13,5 +8,32 @@ angular.module('devMtIn').controller('homeCtrl', function($scope) {
 		display: 'Descending',
 		value: true
 	}];
+
+	$scope.editing = false;
+
+	$scope.saveProfile = function(profile) {
+		profileService.saveProfile(profile);
+		$scope.editing = false;
+	}
+
+	$scope.deleteProfile = function(profile) {
+		profileService.deleteProfile().then(function(deletedProfile) {
+			localStorage.removeItem('profileId');
+			$scope.myProfile = {};
+		}).catch(function(err) {
+			console.error(err);
+		});
+		$scope.myProfile = profileService.checkForProfile();
+		$scope.editing = false;
+	}
+
+	$scope.checkForProfile = function() {
+		var profileId = JSON.parse(localStorage.getItem('profileId'));
+		if(profileId) {
+			profileService.checkForProfile(profileId.profileId).then(function(profile) {
+				$scope.myProfile = profile.data;}).catch(function(err) {console.err(err)});
+		}
+	}
+	$scope.checkForProfile();
 
 });
